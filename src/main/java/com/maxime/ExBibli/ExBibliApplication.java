313572b53855ff7599965b5c8bfa778cf5fd0a3b
maxime <maxime.aumegeas@laplateforme.io>
2025-12-03 13:17:43 +0100

    Creation projet

---
+++ src/main/java/com/maxime/ExBibli/ExBibliApplication.java
@@ -0,0 +1,13 @@
+package com.maxime.ExBibli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExBibliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExBibliApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maxime/ExBibli/ExBibliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maxime.ExBibli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExBibliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
